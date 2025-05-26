663c5599f9a4e6d50141a39215089719a2215f90
levchigintsev <lev.chigintsev@gmail.com>
2025-05-27 00:51:40 +0300

    Add PUT endpoint for updating VerbForms with version checks

--- src/main/kotlin/com/leoschulmann/almi/CORSConfiguration.kt
+++ src/main/kotlin/com/leoschulmann/almi/CORSConfiguration.kt
@@ -1,5 +1,6 @@
 package com.leoschulmann.almi
 
+import io.ktor.http.HttpMethod
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cors.routing.*
 
@@ -11,7 +12,7 @@ fun Application.configureCORS() {
 //        allowHost("myotherdomain.com", schemes = listOf("https"))
 //        allowMethod(HttpMethod.Get)
 //        allowMethod(HttpMethod.Post)
-//        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Put)
 //        allowMethod(HttpMethod.Delete)
 //        allowHeader(HttpHeaders.Authorization)
 //        allowHeader(HttpHeaders.ContentType)
--- src/main/kotlin/com/leoschulmann/almi/api/VerbFormApi.kt
+++ src/main/kotlin/com/leoschulmann/almi/api/VerbFormApi.kt
@@ -1,10 +1,12 @@
 package com.leoschulmann.almi.api
 
 import com.leoschulmann.almi.dto.CreateVerbFormDto
+import com.leoschulmann.almi.dto.UpdateVerbFormDto
 import com.leoschulmann.almi.dto.toDto
 import com.leoschulmann.almi.entities.Verb
 import com.leoschulmann.almi.entities.VerbForm
 import com.leoschulmann.almi.entities.VerbFormTransliteration
+import com.leoschulmann.almi.tables.VerbFormTranslitTable
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
@@ -40,9 +42,62 @@ fun Application.verbFormApi() {
                                 this.verbForm = this@apply
                             })
                         }
-                    }
+                    }.toDto()
                 }
-                call.respond(HttpStatusCode.OK, verbForm.id.value)
+                call.respond(HttpStatusCode.OK, verbForm)
+            }
+
+            put {
+                val dto = call.receive<UpdateVerbFormDto>()
+
+                val entity = transaction {
+                    VerbForm.findById(dto.id)
+                } ?: return@put call.respond(HttpStatusCode.NotFound, "Verb form not found")
+
+                if (dto.version != entity.version) {
+                    return@put call.respond(HttpStatusCode.Conflict, "Version mismatch")
+                }
+
+                val transliterationVersionMap =
+                    transaction {
+                        VerbFormTransliteration.find { VerbFormTranslitTable.verbForm eq entity.id }
+                            .associate { it.id.value to it.version }
+                    }
+                        
+
+                if (!dto.updateTransliterations.all {
+                        transliterationVersionMap.containsKey(it.id) && transliterationVersionMap[it.id] == it.version
+                    }) {
+                    return@put call.respond(HttpStatusCode.Conflict, "Transliterations version mismatch")
+                }
+
+                val verbFormDto = transaction {
+                    if (dto.value != entity.value) {
+                        entity.value = dto.value
+                        entity.version += 1
+                    }
+
+                    dto.updateTransliterations.forEach { (id, version, value) ->
+                        entity.transliterations.find { it.id.value == id }?.let {
+                            if (it.value != value) {
+                                it.value = value
+                                it.version += 1
+                            }
+                        }
+                    }
+
+                    dto.createTransliterations.forEach { (lang, value) ->
+                        entity.transliterations.plus(VerbFormTransliteration.new {
+                            this.lang = lang
+                            this.value = value
+                            this.verbForm = entity
+                        })
+                    }
+
+                    entity.toDto()
+                }
+
+                return@put call.respond(HttpStatusCode.OK, verbFormDto)
             }
 
             get {
